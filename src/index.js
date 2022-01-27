0373b1d03a9dc2201910c8bf6fccbc00eb6fcfa5
Mr.Vyas <divyeshvyas562@gmail.com>
2022-01-27 16:06:43 +0530

    done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import 'antd/dist/antd.css'
+import '~antd/dist/antd.css'
 
 import { Provider } from 'react-redux';
 import store from './app/store'
