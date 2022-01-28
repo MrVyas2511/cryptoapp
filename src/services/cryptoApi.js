7a4243ac5f1f33b0e94534699615d53e064615af
Mr.Vyas <divyeshvyas562@gmail.com>
2022-01-28 15:54:08 +0530

    Env Added

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,18 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '12e7364f71msh9c9fdffcf533061p1e8913jsnfdab422a2e46'
+    'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
 
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/'
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({ baseUrl }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -2,18 +2,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoNewsHeader = {
     'x-bingapis-sdk': 'true',
-    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-    'x-rapidapi-key': '12e7364f71msh9c9fdffcf533061p1e8913jsnfdab422a2e46'
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+    'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+
 }
 
-const baseUrl = 'https://bing-news-search1.p.rapidapi.com/'
 
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeader });
 
 export const cryptoNewsApi = createApi({
     reducerPath: 'cryptoNewsApi',
-    baseQuery: fetchBaseQuery({ baseUrl }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_NEWS_API_URL }),
     endpoints: (builder) => ({
         getCryptoNews: builder.query({
             query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
